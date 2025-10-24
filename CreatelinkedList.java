31efd23ae4c60b2139e6125dbab3c6ae94711720
AnkitKumar <ankitak5285@gmail.com>
2025-10-24 11:44:17 +0530

    CreateLinkedList

---
+++ CreatelinkedList.java
@@ -0,0 +1,15 @@
+public class CreatelinkedList{
+    public static class Node{
+        int data;
+        Node next;//node type ka next variable
+
+        //constructor
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
+}
