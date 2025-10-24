edff9792260103606e4fbc81feee9573cd6b091c
AnkitKumar <ankitak5285@gmail.com>
2025-10-24 12:03:15 +0530

    add last in LinkedList

--- AddLinkedList.java
+++ AddLinkedList.java
@@ -11,6 +11,7 @@ public class AddLinkedList {
     public static Node head;
     public static Node tail;
 
+    //add first in linked list
     public void addFirst(int data){
         //create a new node
         Node newNode = new Node(data);
@@ -22,10 +23,24 @@ public class AddLinkedList {
         head = newNode;
     }
 
+    //add last in linkedlist
+    public void addLast(int data){
+        Node newNode = new Node(data);
+        if(head==null){
+            head=tail=newNode;
+            return;
+        }
+        tail.next = newNode;
+        tail = newNode;
+    }
+
     public static void main(String[] args) {
         AddLinkedList ll = new AddLinkedList();
         ll.addFirst(0);
         ll.addFirst(1);
         ll.addFirst(2);
+        ll.addLast(3);
+        ll.addLast(4);
+        //output : 2->1->0->3->4
     }
 }
